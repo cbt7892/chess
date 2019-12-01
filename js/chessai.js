01b236fb16c499c69e80232ebb718b418735733d
Cole Tramel <cole.b.tramel@gmail.com>
2019-11-30 20:06:43 -0600

    Testing no mobile scroll

--- js/chessai.js
+++ js/chessai.js
@@ -1,6 +1,16 @@
 var board = null
 var game = new Chess()
 
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event)
+{
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    event.stopPropagation();
+}
+
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
